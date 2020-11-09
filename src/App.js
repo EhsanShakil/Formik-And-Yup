949cd728a90358430a61b897d83ce3b8b74517a4
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-11-09 23:48:21 +0500

    Practising Formik and Yup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useFormik } from "formik";
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validate: (values) => {
+      const error = {};
+      if (!values.firstName) {
+        error.firstName = "field is required";
+      } else if (values.firstName.length > 20) {
+        error.firstName = "no more than 20 characters";
+      }
+      if (!values.lastName) {
+        error.lastName = "field is required";
+      } else if (values.lastName.length > 20) {
+        error.lastName = "no more than 20 characters";
+      }
+      return error;
+    },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            id="firstName"
+            value={formik.values.firstName}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.firstName ? <p>{formik.errors.firstName}</p> : null}
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            id="lastName"
+            value={formik.values.lastName}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.lastName ? <p>{formik.errors.lastName}</p> : null}
+        </div>
+        <button type="button">Submit</button>
+      </form>
     </div>
   );
 }
